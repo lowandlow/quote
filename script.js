af6ed6f8802ee4c53825f8d054b5ffcf825023d8
Pratheek M <dflpratheekm@gmail.com>
2025-10-26 20:20:20 +0530

    Fix API URL in generateQuote function

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ async function generateQuote(){
   const quoteElement=document.getElementById('quote');
   try{
     //Fetch quote from quotable api
-  const response=await fetch('https://api.quoteable.io/random');
+  const response=await fetch('https://api.quotable.io/random');
     if(!response.ok){
       throw new Error('Failed to fetch quote');
     }
