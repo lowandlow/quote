dad243706d8221d7033ef3c46594dfffe0809e32
Pratheek M <dflpratheekm@gmail.com>
2025-10-26 20:03:41 +0530

    Add async function to fetch random quotes

---
+++ script.js
@@ -0,0 +1,15 @@
+async function generateQuote(){
+  const quoteElement=document.getElementById('quote');
+  try{
+    //Fetch quote from quotable api
+  const response=await fetch('https://api.quoteable.io/random');
+    if(!response.ok){
+      throw new Error('Failed to fetch quote');
+    }
+    const data=await response.json();
+    quoteElement.innerText="${data.content}"\n- ${data.author};
+  }
+  catch(error){
+    quoteElement.innerText="Could not fecth quote";
+    console.error(error);
+  }}
